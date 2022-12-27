b3082d42ef46325ae3b9faf1eabe7f207ea080ce
Stanislav Stoyanov <slavkata_99@abv.bg>
2022-12-27 22:21:55 +0200

    Add initial console app

---
+++ ElGamalAlgorithm/ElGamalAlgorithm/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ElGamalAlgorithm
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
